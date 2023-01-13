99ea59e40b9e39bba7d271db19d9d7dc6472cedb
Satronic <electronsgm@gmail.com>
2023-01-12 18:34:17 -1200

    FEAT(HomeScreen.tsx): Add the GET request to get the list of movies from the API

---
+++ src/api/movieApi.tsx
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const movieAPI = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/movie',
+    params: {
+        api_key: 'dc3c672769ed631c8ef9cf3cbcfb39ac',
+        lanaguage: 'es-ES'
+    }
+});
+
+export default movieAPI;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import movieAPI from '../api/movieApi';
 
 export const HomeScreen = () => {
+
+  useEffect(() => {
+    movieAPI.get('/now_playing')
+      .then(response => console.log(JSON.stringify(response.data, null, 4)))
+  }, [])
+
   return (
     <View>
-        <Text>Home Screen</Text>
+      <Text>Home Screen</Text>
     </View>
   )
 }
