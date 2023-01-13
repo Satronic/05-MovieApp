b5c451584184f3bd4f20efe7a777955f76766505
Satronic <electronsgm@gmail.com>
2023-01-12 17:33:22 -1200

    FEAT(App.tsx): Add the NavigationContainer component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text } from 'react-native';
+
 
 const App = () => {
   return (
-    <View>
-      <Text>Movie App</Text>
-    </View>
+    <NavigationContainer>
+        <Text>Hola</Text>
+    </NavigationContainer>
   )
 }
 
