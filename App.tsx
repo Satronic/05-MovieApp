dd51ff1310290d59b8e3771958886a3ea6220e10
Satronic <electronsgm@gmail.com>
2023-01-12 16:09:06 -1200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Movie App</Text>
+    </View>
+  )
+}
+
+export default App;
