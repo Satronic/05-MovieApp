28ccab8171eda4130e43ab548433c34ca82cb17d
Satronic <electronsgm@gmail.com>
2023-01-12 18:59:27 -1200

    FEAT(/interfaces): Add the data types for the GET request response

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import movieAPI from '../api/movieApi';
+import { MovieApiNowPlaying } from '../interfaces/movieApiInterface';
 
 export const HomeScreen = () => {
 
   useEffect(() => {
-    movieAPI.get('/now_playing')
-      .then(response => console.log(JSON.stringify(response.data, null, 4)))
+    movieAPI.get<MovieApiNowPlaying>('/now_playing')
+      .then(response => console.log(JSON.stringify(response.data.results, null, 4)))
   }, [])
 
   return (
